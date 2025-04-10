96561b1ca21e6bb4a297e3b2ebf15ddcfa01e23e
ChrDuus <duus92@web.de>
2025-04-10 12:47:01 +0200

    Filestructure created

---
+++ src/app/main-content/contact/contact-overlay/contact-overlay.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-contact-overlay',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './contact-overlay.component.html',
+  styleUrl: './contact-overlay.component.scss'
+})
+export class ContactOverlayComponent {
+
+}
