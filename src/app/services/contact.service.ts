1f6cc737e77e0a35494dd5bfa0ad91f104abcdfc
Soufiane Nouira <soufianenouira@gmail.comm>
2025-04-10 21:49:29 +0200

    Contact list render and sort functions done

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { inject } from '@angular/core';
 import { Firestore, collectionData, collection, doc, onSnapshot, addDoc} from '@angular/fire/firestore';
 import { Contact } from '../interfaces/contact';
+import { query, orderBy, limit } from 'firebase/firestore'; 
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,19 +13,50 @@ export class ContactService {
   firestore:Firestore = inject(Firestore);
   /* contacts  = collectionData(this.getContactsRef()); */
   contactList: Contact[] = [];
+  contactListLaters: number[] = [];
+  contactListLater: string[] = [];
   constructor() {
     this.snap();
+    this.checkContactListLaters();
   }
 
   snap(){
-    this.unsubContactList = onSnapshot(this.getContactsRef(), (list)=>{
+    let q = query(this.getContactsRef(), orderBy('name'));
+    this.unsubContactList = onSnapshot(q, (list)=>{
       this.contactList= [];
       list.forEach(element => {
-        this.contactList.push(this.setContactObj(element.data(), element.id));
-        
-        console.log(this.contactList);
+          this.contactList.push(this.setContactObj(element.data(), element.id));
       })
+      this.checkContactListLaters();
     });
+  }
+
+  checkContactListLaters(){
+    let latter = 0;
+    
+    let condition = true;
+    let startValue = 0;
+    this.contactList.forEach(contact =>{
+      while (condition) {
+        if(contact.name.startsWith(String.fromCharCode(latter + 65))){
+          this.contactListLaters[latter] = startValue + 1 ;
+          if(!this.contactListLater.includes(String.fromCharCode(latter + 65)))
+          this.contactListLater.push(String.fromCharCode(latter + 65))
+          condition = false;
+        }else{
+          latter++;
+        }
+
+        if(latter == 26){
+          condition = false;
+        }
+      }
+      condition = true;
+      latter = 0;
+      console.log(this.contactListLater);
+      
+    });
+
   }
 
   ngonDestroy(){
@@ -37,6 +70,7 @@ export class ContactService {
       (docRef)=>{ console.log("doc", docRef?.id);
       }
     );
+    this.checkContactListLaters();
   }
 
   setContactObj(obj: any, id: string):Contact{
