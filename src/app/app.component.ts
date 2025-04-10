0384fa95fb5376e556457b10e40031bcab9ebef0
MihaelaAghirculesei <aghirculesei@gmail.com>
2025-04-10 17:24:46 +0200

    Structure of header folder added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContactOverlayComponent } from './main-content/contact/contact-overlay/contact-overlay.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContactOverlayComponent],
+  imports: [CommonModule, RouterOutlet, ContactOverlayComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
