b62aac2fb2935210333bb443c9938b97aabc5f7d
ChrDuus <duus92@web.de>
2025-04-10 17:00:27 +0200

    Add contact Overlay added

--- src/app/main-content/contact/contact-overlay/contact-overlay.component.ts
+++ src/app/main-content/contact/contact-overlay/contact-overlay.component.ts
@@ -1,13 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild,  } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormsModule, NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-overlay',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact-overlay.component.html',
-  styleUrl: './contact-overlay.component.scss'
+  styleUrl: './contact-overlay.component.scss',
 })
 export class ContactOverlayComponent {
+  isOpen: boolean = true;
 
+  contactData: {
+    name: string;
+    email: string;
+    phone: string;
+  } = {
+    name: '',
+    email: '',
+    phone: '',
+  };
+  invalidFields: string[] = [];
+
+  validateForm(field: string) {
+    this.invalidFields = this.invalidFields.filter(f => f !== field);
+    if(field === 'name'){
+    if (!this.contactData.name || this.contactData.name.length < 3) {
+      this.invalidFields.push('name');
+      this.contactData.name = ''; 
+    }
+  }
+    if(field === 'email'){
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!this.contactData.email || !emailRegex.test(this.contactData.email)) {
+      this.invalidFields.push('email');      
+      this.contactData.email = ''; 
+    }
+  }
+  if( field === 'phone'){
+    const phoneRegex = /^(?:\+?\d{1,4}[ \-]?)?(\(?\d{1,4}\)?[ \-]?)?[\d\- ]{5,15}$/;
+    if (!this.contactData.phone || !phoneRegex.test(this.contactData.phone)) {
+      this.invalidFields.push('phone');
+      this.contactData.phone = ''; 
+    }
+  }   
+  }
+
+  isFormValid(): boolean {
+    return (
+      this.contactData.name.length >= 3 &&
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(this.contactData.email) &&
+      /^(?:\+?\d{1,4}[ \-]?)?(\(?\d{1,4}\)?[ \-]?)?[\d\- ]{5,15}$/.test(this.contactData.phone)
+    );
+  }
+
+  toggleOverlay() {
+    this.isOpen = !this.isOpen;
+  }
 }
