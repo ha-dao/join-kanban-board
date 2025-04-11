5f462b53690223322b627398691e083cf8ddfad5
MihaelaAghirculesei <aghirculesei@gmail.com>
2025-04-11 15:25:54 +0200

    Added Header Dropdown Menu

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+ showSubMenu = false;
+
+  toggleSubMenu(event: MouseEvent) {
+    event.stopPropagation(); // Verhindert, dass der Klick auch im "overlay" landet
+    this.showSubMenu = !this.showSubMenu;
+  }
+
+  closeSubMenu() {
+    this.showSubMenu = false;
+  }
+
+  onSubMenuClick(event: MouseEvent) {
+    event.stopPropagation(); // Klicks im Sub-Menü sollen es nicht schließen
+  }
+
+  logoutCurrentUser() {
+    console.log("User logged out");
+    this.closeSubMenu();
+  }
 }
