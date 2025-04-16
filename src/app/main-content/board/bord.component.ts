eef69ed82c9d9b5203d3e35bf1f3178f5ee0a9e2
Marvin Schneemann <marvinschneemann@gmx.de>
2025-04-16 13:30:27 +0200

    route sidebar

---
+++ src/app/main-content/board/bord.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bord',
+  standalone: true,
+  imports: [],
+  templateUrl: './bord.component.html',
+  styleUrl: './bord.component.scss'
+})
+export class BordComponent {
+
+}
