1dc291742f51944a4fe9d02c76d7f51d22eee33b
Marvin Schneemann <marvinschneemann@gmx.de>
2025-04-16 13:30:27 +0200

    route sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,21 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-//   {
-//     path: '', redirectTo: 'summary', pathMatch: 'full',
-//   },
-//   {
-//     path: 'summary', loadComponent: () => import('./pages/summary/summary.component').then((m) => m.SummaryComponent),
-//   },
-//   {
-//     path: 'add-task',loadComponent: () => import('./pages/add-task/add-task.component').then( (m) => m.AddTaskComponent),
-//   },
-//   {
-//     path: 'board', loadComponent: () => import('./pages/board/board.component').then((m) => m.BoardComponent),
-//   },
-//   {
-//     path: 'contacts',loadComponent: () => import('./pages/contacts/contacts.component').then((m) => m.ContactsComponent),
-//   },
+  {
+    path: '', redirectTo: 'summary', pathMatch: 'full',
+  },
+  {
+    path: 'summary', loadComponent: () => import('./main-content/summary/summary.component').then((m) => m.SummaryComponent),
+  },
+  {
+    path: 'add-task',loadComponent: () => import('./main-content/add-task/add-task.component').then( (m) => m.AddTaskComponent),
+  },
+  {
+    path: 'board', loadComponent: () => import('./main-content/board/bord.component').then((m) => m.BordComponent),
+  },
+  {
+    path: 'contacts',loadComponent: () => import('./main-content/contact/contact.component').then((m) => m.ContactComponent),
+  },
 //   {
 //     path: 'privacy-policy', loadComponent: () => import('./pages/privacy-policy/privacy-policy.component').then((m) => m.PrivacyPolicyComponent),
 //   },
