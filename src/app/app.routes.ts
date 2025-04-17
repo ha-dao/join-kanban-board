96e0091e661967d1d98b246569db9196f586c261
Marvin Schneemann <marvinschneemann@gmx.de>
2025-04-17 10:10:22 +0200

    Routing finish

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,37 +5,45 @@ import { ContactComponent } from './main-content/contact/contact.component';
 import { HelpComponent } from './shared/components/header/help/help.component';
 
 export const routes: Routes = [
-    {
-        path: '', component: ContactComponent
-    },
-    {
-        path: 'legal-notice', component: LegalNoticeComponent
-    },
-    {
-        path: 'privacy', component: PrivacyPolicyComponent
-    },
   {
-    path: '', redirectTo: 'summary', pathMatch: 'full',
+    path: '',
+    component: ContactComponent,
   },
   {
-    path: 'summary', loadComponent: () => import('./main-content/summary/summary.component').then((m) => m.SummaryComponent),
+    path: 'legal-notice',
+    component: LegalNoticeComponent,
   },
   {
-    path: 'add-task',loadComponent: () => import('./main-content/add-task/add-task.component').then( (m) => m.AddTaskComponent),
+    path: 'privacy',
+    component: PrivacyPolicyComponent,
   },
   {
-    path: 'board', loadComponent: () => import('./main-content/board/board.component').then((m) => m.BordComponent),
+    path: 'summary',
+    loadComponent: () =>
+      import('./main-content/summary/summary.component').then(
+        (m) => m.SummaryComponent
+      ),
   },
   {
-    path: 'contact',loadComponent: () => import('./main-content/contact/contact.component').then((m) => m.ContactComponent),
+    path: 'add-task',
+    loadComponent: () =>
+      import('./main-content/add-task/add-task.component').then(
+        (m) => m.AddTaskComponent
+      ),
   },
-//   {
-//     path: 'privacy-policy', loadComponent: () => import('./pages/privacy-policy/privacy-policy.component').then((m) => m.PrivacyPolicyComponent),
-//   },
-//   {
-//     path: 'legal-notice',loadComponent: () => import('./pages/legal-notice/legal-notice.component').then((m) => m.LegalNoticeComponent),
-//   },
-
-{ path: 'help', component: HelpComponent }, 
+  {
+    path: 'board',
+    loadComponent: () =>
+      import('./main-content/board/board.component').then(
+        (m) => m.BordComponent
+      ),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./main-content/contact/contact.component').then(
+        (m) => m.ContactComponent
+      ),
+  },
+  { path: 'help', component: HelpComponent },
 ];
-
