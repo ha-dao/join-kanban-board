48cebfae688c550cf9f8304ab1cde477ce89adcc
ChrDuus <duus92@web.de>
2025-04-22 09:25:28 +0200

    Overlay reworked in Service

--- src/app/main-content/contact/contact-overlay/contact-overlay.component.ts
+++ src/app/main-content/contact/contact-overlay/contact-overlay.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormControl, FormsModule, NgForm, NgModel } from '@angular/forms';
 import { ContactService } from '../../../services/contact.service';
 import { FeedbackServiceService } from '../../../services/feedback.service';
+import { ContactOverlayService } from '../../../services/overlay.service';
 
 @Component({
   selector: 'app-contact-overlay',
@@ -12,14 +13,12 @@ import { FeedbackServiceService } from '../../../services/feedback.service';
   styleUrl: './contact-overlay.component.scss',
 })
 export class ContactOverlayComponent {
-  @Input() isOpen: boolean = false;
-  @Output() closeOverlay = new EventEmitter<void>();
-  constructor(public contactService: ContactService, public feedbackService: FeedbackServiceService){
+ 
+  constructor(public contactService: ContactService, public feedbackService: FeedbackServiceService, public overlayService: ContactOverlayService){
 
 
   }
-  buttonLeft: string = 'Cancel';
-  buttonRight: string = 'Create Contact';
+  @ViewChild('overlayRef') overlayRef!: ElementRef;
   contactData: {
     name: string;
     email: string;
@@ -32,26 +31,44 @@ export class ContactOverlayComponent {
   invalidFields: string[] = [];
   showSuccessMessage:boolean = false;
 
+  ngOnInit() {
+    this.overlayService.contactData$.subscribe(data => {
+      if (data) {
+        this.contactData = { ...data }; 
+      }
+    });
+  }
+
+  handleBackdropClick(event: MouseEvent) {
+    const clickedInside = this.overlayRef.nativeElement.contains(event.target);
+    if (!clickedInside && this.overlayService.isOpen) {
+      this.clearForm();
+      this.overlayService.closeOverlay();
+    }
+  }
+  
+
   validateForm(field: string) {
     this.invalidFields = this.invalidFields.filter(f => f !== field);
     if(field === 'name'){
     if (!this.contactData.name || this.contactData.name.length < 2) {
       this.invalidFields.push('name');
-      this.contactData.name = '';
+      
     }
   }
     if(field === 'email'){
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     if (!this.contactData.email || !emailRegex.test(this.contactData.email)) {
       this.invalidFields.push('email');
-      this.contactData.email = '';
+      
     }
   }
   if( field === 'phone'){
     const phoneRegex = /^(?:\+?\d{1,4}[ \-]?)?(\(?\d{1,4}\)?[ \-]?)?[\d\- ]{5,15}$/;
     if (!this.contactData.phone || !phoneRegex.test(this.contactData.phone)) {
       this.invalidFields.push('phone');
-      this.contactData.phone = '';
+      
+      
     }
   }
   }
@@ -64,47 +81,41 @@ export class ContactOverlayComponent {
     );
   }
 
-  toggleOverlay(buttonLeft: string, buttonRight: string) {
-    this.isOpen = !this.isOpen;
-    this.toggleBodyScroll(this.isOpen);
-    this.buttonLeft = buttonLeft;
-    this.buttonRight = buttonRight;
-
-  }
+  
 
 
   cancelOrDelete(){
-    if(this.buttonLeft == 'Delete'){
+    if(this.overlayService.buttonLeft == 'Delete'){
       this.contactService.deleteContact();
-      this.feedbackService.show('Kontakt erfolgreich gelöscht!');
+      this.feedbackService.show('Contact successfully deleted!');
     }
-    this.toggleOverlay('', '');
+    this.overlayService.closeOverlay();
     this.clearForm();
     
   }
 
   addOrSave(){
-    if(this.buttonRight == 'Create Contact'){
+    if(this.overlayService.buttonRight == 'Create Contact'){
       this.contactService.addContact(this.contactData);
       this.clearForm();
            
-      this.toggleOverlay('', '')
-      this.feedbackService.show('Kontakt erfolgreich angelegt!');
-    }else if(this.buttonRight == 'Save'){
+      this.overlayService.closeOverlay()
+      this.feedbackService.show('Contact added!');
+    }else if(this.overlayService.buttonRight == 'Save'){
       this.contactService.updateContact(this.contactData);
-      this.feedbackService.show('Kontakt erfolgreich geändert!');
+      this.feedbackService.show('Contact changed!');
       this.clearForm();
-      this.toggleOverlay('','')
+      this.overlayService.closeOverlay()
     }
   }
 
-  toggleBodyScroll(disable: boolean) {
-    if (disable) {
-      document.body.classList.add('no-scroll');
-    } else {
-      document.body.classList.remove('no-scroll');
-    }
-  }
+  // toggleBodyScroll(disable: boolean) {
+  //   if (disable) {
+  //     document.body.classList.add('no-scroll');
+  //   } else {
+  //     document.body.classList.remove('no-scroll');
+  //   }
+  // }
 
   clearForm(){
     this.contactData.name= '';
--- src/app/main-content/contact/single-contact/single-contact.component.ts
+++ src/app/main-content/contact/single-contact/single-contact.component.ts
@@ -1,8 +1,10 @@
-import { Component, Input, Output, EventEmitter, ViewChild, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { ContactService } from '../../../services/contact.service';
 import { CommonModule } from '@angular/common';
 import { ContactOverlayComponent } from '../contact-overlay/contact-overlay.component';
 import { FeedbackServiceService } from '../../../services/feedback.service';
+import { ContactOverlayService } from '../../../services/overlay.service'; 
+
 @Component({
   selector: 'app-single-contact',
   standalone: true,
@@ -11,22 +13,26 @@ import { FeedbackServiceService } from '../../../services/feedback.service';
   styleUrl: './single-contact.component.scss'
 })
 export class SingleContactComponent {
-  constructor(public feedbackService : FeedbackServiceService){
+  constructor(
+    public feedbackService: FeedbackServiceService,
+    private overlayService: ContactOverlayService
+  ) {}
 
-  }
   contactService = inject(ContactService);
-  @ViewChild('editContactOverlay') contactOverlay!: ContactOverlayComponent;
-  
-
-  deleteContact(){
-      this.contactService.deleteContact();
-      this.feedbackService.show('Kontakt erfolgreich gelöscht!');
+  @ViewChild('editContactOverlay') contactOverlay!: ContactOverlayComponent;  deleteContact() {
+    this.contactService.deleteContact();
+    this.feedbackService.show('Contact successfully deleted!');
   }
 
-  editContact(){
-    this.contactOverlay.toggleOverlay('Delete', 'Save');
-    this.contactOverlay.contactData.name = this.contactService.currentContact.name;
-    this.contactOverlay.contactData.email = this.contactService.currentContact.email;
-    this.contactOverlay.contactData.phone = this.contactService.currentContact.phone;
+  editContact() {
+    this.overlayService.setOverlayButtons('Delete', 'Save');
+  
+    this.overlayService.setContactData({
+      name: this.contactService.currentContact.name,
+      email: this.contactService.currentContact.email,
+      phone: this.contactService.currentContact.phone
+    });
+  
+    this.overlayService.openOverlay();
   }
 }
