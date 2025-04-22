8204d46ac95c539ae12e44411e17b3e94b623f92
Ha Dao <dcvh@protonmail.com>
2025-04-22 11:30:12 +0200

    more more changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
 import { HeaderComponent } from './shared/components/header/header.component';
 import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 import { FeedbackServiceService } from './services/feedback.service';
 import { ContactOverlayService } from './services/overlay.service';
+import { ContactOverlayComponent } from './main-content/contact/contact-overlay/contact-overlay.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContactComponent, HeaderComponent, SidebarComponent, ContactOverlayComponent ],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, SidebarComponent, ContactOverlayComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
