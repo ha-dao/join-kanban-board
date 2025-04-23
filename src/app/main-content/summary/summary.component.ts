25b87cf2bf627fe39eae6b24432195906279ba88
Marvin Schneemann <marvinschneemann@gmx.de>
2025-04-23 16:13:28 +0200

    struktur for summary

--- src/app/main-content/summary/summary.component.ts
+++ src/app/main-content/summary/summary.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-summary',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss'
 })
 export class SummaryComponent {
 
+  userName = signal('Sofia Müller');
+
+  greeting = computed(()=> {
+
+    const hour = new Date().getHours();
+
+    if (hour < 12) return 'Good morning,';
+    if (hour < 18) return 'Good afternoon,';
+    return 'Good evening,'
+
+  });
 }
