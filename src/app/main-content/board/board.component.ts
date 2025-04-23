5af440a5db5d771295c613ddff593dba3e8071e4
MihaelaAghirculesei <aghirculesei@gmail.com>
2025-04-23 14:44:50 +0200

    Board created

--- src/app/main-content/board/board.component.ts
+++ src/app/main-content/board/board.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-bord',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './board.component.html',
-  styleUrl: './board.component.scss'
+  styleUrl: './board.component.scss',
 })
 export class BordComponent {
+  columns = ['To Do', 'In Progress', 'Await Feedback', 'Done'];
+  isActive = false; 
+  searchQuery = ''; 
 
+
+  isAddTaskHovered = false;
+  isPlusButtonHovered: boolean[] = Array(this.columns.length).fill(false); 
+  toggleActive() {
+    this.isActive = false; 
+    this.searchQuery = '';
+  }
+
+  onInputChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.searchQuery = input.value; 
+
+    this.isActive = this.searchQuery.length > 0;
+  }
 }
