aee93fe4c87df0bc777c336a6ae8a47d23fa0fce
MihaelaAghirculesei <aghirculesei@gmail.com>
2025-05-08 13:36:33 +0200

    Add header/footer and refactor button mixins for Log In

--- src/app/landingpage/login/login.component.ts
+++ src/app/landingpage/login/login.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   email = '';
   password = '';
+
+  constructor(private routerModule: RouterModule, private router: Router) {}
+
+  goToAnotherPage(target:string): void {
+    this.router.navigate([target]);
+  }
 
   login() {
     alert(`E-Mail: ${this.email}\nPasswort: ${this.password}`);
